6a0b4c1f70efcc789c78aca20eb0e68318d5c54a
Weslley David <weslleyvwp@hotmail.com>
2023-04-25 09:15:17 -0300

    feat: adding create and delete

--- b3/b3investmanager/urls.py
+++ b3/b3investmanager/urls.py
@@ -3,12 +3,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # indexes
     path("", views.index, name="index"),
 
     path("code_index/", views.code_index, name="code_index"),
-    
+
     path("investment_index/", views.investment_index, name="investment_index"),
+
+    # addictions
+    path("add_code/", views.add_code, name="add_code"),
+
+    path("add_investment/", views.add_investment, name="add_investment"),
+
+    path('code/<int:pk>/delete/', views.delete_code, name='delete-code'),
     
+    path('investment/<int:pk>/delete/', views.delete_investment, name='delete-investment'),
+
     # ex: /polls/5/
     path("<int:question_id>/", views.detail, name="detail"),
     # ex: /polls/5/results/
--- b3/b3investmanager/views.py
+++ b3/b3investmanager/views.py
@@ -1,5 +1,4 @@
-from unittest import loader
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 from django.http import Http404, HttpResponse
@@ -7,9 +6,10 @@ from .models import Code, Investment
 
 
 def index(request):
-    return HttpResponse('welcome to b3invest manager')
+    return render(request, "b3investmanager/index.html")
 
 
+# list of codes and investments
 def code_index(request):
     codes = Code.objects.all()
     context = {"codes": codes}
@@ -21,6 +21,55 @@ def investment_index(request):
     context = {"investments": investments}
     return render(request, "b3investmanager/investment_index.html", context)
 
+# create investment
+
+
+def add_code(request):
+    if request.method == 'POST':
+        code_identifyer = request.POST['code_identifyer']
+        code = Code(code_identifyer=code_identifyer)
+        code.save()
+        return redirect('code_index')
+        # Redirect to success page
+    else:
+        # Render empty form
+        return render(request, 'b3investmanager/add_code.html')
+
+
+def add_investment(request):
+    if request.method == 'POST':
+        investment = Investment(
+            date=request.POST['date'],
+            investment_identifyer=request.POST['investment_identifyer'],
+            Code=Code.objects.get(
+                code_identifyer=request.POST['code_identifyer']),
+            quantity=request.POST['quantity'],
+            value_unit=request.POST['value_unit'],
+            operation=request.POST['operation'],
+            value_brokerage=request.POST['value_brokerage'],
+            value_operation=request.POST['value_operation'],
+            b3_rate=request.POST['b3_rate'],
+            value_total=request.POST['value_total']
+        )
+        investment.save()
+        # Redirect to success page
+        return redirect('investment_index')
+        # Redirect to success page
+    else:
+        # Render empty form
+        codes = Code.objects.all()
+        return render(request, 'b3investmanager/add_investment.html', {'codes': codes})
+
+
+def delete_code(request, pk):
+    code = Code.objects.get(pk=pk)
+    code.delete()
+    return redirect('code_index')
+
+def delete_investment(request, pk):
+    investment = Investment.objects.get(pk=pk)
+    investment.delete()
+    return redirect('investment_index')
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
